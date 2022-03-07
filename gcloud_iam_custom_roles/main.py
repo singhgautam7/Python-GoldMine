a751584d8f893b08ba6894e1e7134cf12df6d09c
Gautam Singh <gautamsingh1997@gmail.com>
2022-03-07 19:25:10 +0530

    added some colors in print function

--- gcloud_iam_custom_roles/main.py
+++ gcloud_iam_custom_roles/main.py
@@ -10,6 +10,24 @@ service = discovery.build('iam', 'v1', credentials=credentials)
 parent = f'projects/{config.PROJECT_ID}' if config.flag_is_a_project else f'organizations/{config.ORG_ID}'
 
 
+class Colors:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    VIOLET = '\33[35m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ITALIC = '\33[3m'
+    URL = '\33[4m'
+    BLINK = '\33[5m'
+    SELECTED = '\33[7m'
+
+    END = '\033[0m'
+
+
 class HelperGCloud:
     """
     Helper class which will interact with gcloud.
@@ -104,20 +122,20 @@ class HelperGCloud:
 
 if __name__ == '__main__':
 
-    print("Please wait. Getting Role Info")
+    print(f'{Colors.GREEN}Please wait. Getting Role Info.......{Colors.END}')
 
     # Get the role info
     init_role_info = HelperGCloud.get_role_info()
 
     # If the role info is empty, then create a role
     if not bool(init_role_info):
-        print(f'\nNo role found with ID {config.ROLE_ID}.\nCREATING ROLE')
+        print(f'\n{Colors.WARNING}No role found with ID {config.ROLE_ID}.{Colors.BOLD} CREATING ROLE{Colors.END}')
         role_info = HelperGCloud.create_role()
 
     # Else, update the role
     else:
-        print(f'\nRole found with ID {config.ROLE_ID}.\nUPDATING ROLE')
+        print(f'\n{Colors.VIOLET}Role found with ID {config.ROLE_ID}.{Colors.BOLD} UPDATING ROLE{Colors.END}')
         role_info = HelperGCloud.update_role()
 
-    print(f'\n\nThe {"NEW" if not bool(init_role_info) else "UPDATED"} role is - ')
+    print(f'\n{Colors.BLUE}The {"NEW" if not bool(init_role_info) else "UPDATED"} role is - {Colors.END}')
     print(json.dumps(role_info, indent=4))
