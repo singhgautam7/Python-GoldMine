ad16a2b4b9db907872083ea7bd6dc2846251d44a
Gautam Singh <gautamsingh1997@gmail.com>
2022-04-09 23:19:09 +0530

    changed the profiler to the decorator

--- python_profiler/example.py
+++ python_profiler/example.py
@@ -1,23 +1,26 @@
 # Question 1: sort list without creating another list as per give order
 # input list=[12345,"Happy","very",1234,"I","am",1,12,123,"excited" ]
 # output list=[1,12,123,1234,12345,"I","am","very","Happy","excited"]
+from python_profiler import main
+
 
 def key_function(x):
     if isinstance(x, int):
-        return 
-    
+        return
+
+
+@main.calculate_total_time_to_run
 def custom_sort(ip_list):
     return sorted(
-        ip_list, 
-        key=lambda x: 
-            (str(x).isalpha(), len(str(x))),
-        )
+        ip_list,
+        key=lambda x:
+        (str(x).isalpha(), len(str(x))),
+    )
 
 
 if __name__ == '__main__':
-    import main
-    
-    ip = [12345,"Happy","very",1234,"I","am",1,12,123,"excited"]
+
+    ip = [12345, "Happy", "very", 1234, "I", "am", 1, 12, 123, "excited"]
     print("input", ip)
-    main.print_total_time_to_run(custom_sort, ip)
-    main.print_time_breakdown(custom_sort, ip)
+    custom_sort(ip)
+    # main.print_time_breakdown(custom_sort, ip)
